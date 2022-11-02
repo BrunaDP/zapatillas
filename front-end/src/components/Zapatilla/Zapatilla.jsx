35934a5e98a74b7ace352248f25d0eafba9ae816
BrunaDP <florenza542@gmail.com>
2022-11-01 23:09:04 -0300

    status

--- front-end/src/components/Zapatilla/Zapatilla.jsx
+++ front-end/src/components/Zapatilla/Zapatilla.jsx
@@ -2,17 +2,31 @@ import React from 'react'
 import { image } from "../../export-de-imagenes";
 import "./Zapatilla.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { faStar, faHeart } from '@fortawesome/free-solid-svg-icons'
 const Zapatilla = () => {
     return (
         <div className='pagina_producto'>
             <h1 className='nome_product'>Products</h1>
-            <div className='container'>
-                <a href=""><div className='container_background'></div></a>
-                <span className='text_1'>Nike<FontAwesomeIcon icon={faStar} className="star" /></span>
+            <div className='card'>
+                <a href="">
+                    <div className='container_background'>
+                        <div className='cuadrado'>
+
+                            <FontAwesomeIcon icon={faHeart} className="corazon" />
+                        </div>
+                        <div className='cuadrado_2'>
+                        </div>
+                    </div>
+                </a>
+                <span className='text_1'>Nike
+                    <div> <FontAwesomeIcon icon={faStar} className="star" />
+                        <FontAwesomeIcon icon={faStar} className="star" />
+                        <FontAwesomeIcon icon={faStar} className="star" />
+                        <FontAwesomeIcon icon={faStar} className="star" />
+                        <FontAwesomeIcon icon={faStar} className="star" /></div></span>
                 <div className='box_2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid temporibus </div>
-                <span className='box_4'>$100,99</span>
-                <div className='box_3'><span className='nome_3'>Add to the cart</span></div>
+                <span className='box_4'>U$S100.99</span>
+                <span><h1 id='box_3'>Add to the cart</h1></span>
 
                 {/*<img src={image.red_shoes1} className="image"></img>*/}
             </div>
