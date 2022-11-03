c91cc3ba9fea26a4528f988bff207564e385eea7
BrunaDP <florenza542@gmail.com>
2022-11-02 22:15:11 -0300

    agregando cosas

--- front-end/src/components/Zapatilla/Zapatilla.jsx
+++ front-end/src/components/Zapatilla/Zapatilla.jsx
@@ -5,27 +5,24 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar, faHeart } from '@fortawesome/free-solid-svg-icons'
 const Zapatilla = () => {
     return (
-        <div className='pagina_producto'>
+        <div className='nav_product'>
             <h1 className='nome_product'>Products</h1>
-            <div className='card'>
+            <div className='product_card'>
                 <a href="">
                     <div className='container_background'>
                         <div className='cuadrado'>
-
                             <FontAwesomeIcon icon={faHeart} className="corazon" />
-                        </div>
-                        <div className='cuadrado_2'>
                         </div>
                     </div>
                 </a>
-                <span className='text_1'>Nike
+                <span className='product-name'>Nike
                     <div> <FontAwesomeIcon icon={faStar} className="star" />
                         <FontAwesomeIcon icon={faStar} className="star" />
                         <FontAwesomeIcon icon={faStar} className="star" />
                         <FontAwesomeIcon icon={faStar} className="star" />
                         <FontAwesomeIcon icon={faStar} className="star" /></div></span>
-                <div className='box_2'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid temporibus </div>
-                <span className='box_4'>U$S100.99</span>
+                <div className='description'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid temporibus </div>
+                <span className='precio'>U$S100.99</span>
                 <span><h1 id='box_3'>Add to the cart</h1></span>
 
                 {/*<img src={image.red_shoes1} className="image"></img>*/}
